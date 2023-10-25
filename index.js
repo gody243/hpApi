5dbadc214df58312ebd8c22f9a0ffcc34938570e
Godefroy Diakite <godefroy.dkt@gmail.com>
2023-10-25 13:30:50 +0100

    fetch name

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+const userSelect=document.getElementById("userSelect")
 
-fetch(https://hp-api.onrender.com/api/characters)
-.then(res =>res.json())
-.then(users=>users.map(user => 
-    const li=document.createElement('li'))
+fetch("https://hp-api.onrender.com/api/characters")
+.then(response => response.json())
+.then(users => {
+    const ps = users.map((user) => {
+      const p = document.createElement('p');
+      p.value = user.id;
+      p.textContent = user.name;
+      return p;
+    });
+    ps.forEach(p => userSelect.appendChild(p));
+})
